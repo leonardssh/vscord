bcfb1d7d4b20b6004844f067119cb0dac4ac146b
LeonardSSH <leonardssh22@gmail.com>
2021-08-10 02:51:32 +0200

    refactor: use rpc instead of VSCord as config prefix

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ import { workspace } from 'vscode';
 import { WorkspaceExtensionConfiguration } from './environment';
 
 export function getConfig(): WorkspaceExtensionConfiguration {
-	return workspace.getConfiguration('VSCord') as WorkspaceExtensionConfiguration;
+	return workspace.getConfiguration('rpc') as WorkspaceExtensionConfiguration;
 }
