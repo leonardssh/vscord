35b38dd2083410a2ded8016258429eb120a57728
Han Yeong-woo <nix6839@washnix.com>
2022-10-16 14:04:10 +0900

    refactor: use Map constructor

--- src/helpers/getApplicationId.ts
+++ src/helpers/getApplicationId.ts
@@ -2,11 +2,11 @@ import { WorkspaceExtensionConfiguration } from "../config";
 import { CONFIG_KEYS } from "../constants";
 
 export const getApplicationId = (config: WorkspaceExtensionConfiguration) => {
-    const applicationIds = new Map<string, string>();
-
-    applicationIds.set("Code", "782685898163617802");
-    applicationIds.set("Visual Studio Code", "810516608442695700");
-    applicationIds.set("VSCodium", "1031067701474492496");
+    const applicationIds = new Map([
+        ["Code", "782685898163617802"],
+        ["Visual Studio Code", "810516608442695700"],
+        ["VSCodium", "1031067701474492496"]
+    ]);
 
     //TODO: Automatically generate regex
     const match = /(Code|Visual Studio Code|VSCodium)/i.exec(config[CONFIG_KEYS.AppName]);
