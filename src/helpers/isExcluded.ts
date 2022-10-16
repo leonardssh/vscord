b3c4ae021e1c3622be12488c9b179480d9efafda
xhayper <hayper1919@gmail.com>
2022-10-16 13:36:10 +0700

    fix: potential fix for excluded folder appearing

--- src/helpers/isExcluded.ts
+++ src/helpers/isExcluded.ts
@@ -1,12 +1,6 @@
-// https://stackoverflow.com/a/3561711
-const escapeRegex = (text: string) => text.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-
 export const isExcluded = (config: string[], toMatch?: string) => {
     if (!config || !toMatch) return false;
     if (!config.length) return false;
 
-    const ignorePattern = config.map(escapeRegex).join("|");
-    const regex = new RegExp(ignorePattern, "gm");
-    const excluded = regex.test(toMatch);
-    return excluded;
+    return config.some((pattern) => new RegExp(pattern, "gm").test(toMatch));
 };
