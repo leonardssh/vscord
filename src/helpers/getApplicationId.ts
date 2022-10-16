e9ab08161da80d4dbbf2347b063815b3c7221b59
xhayper <hayper1919@gmail.com>
2022-10-16 12:45:27 +0700

    feat: properly escape regex

--- src/helpers/getApplicationId.ts
+++ src/helpers/getApplicationId.ts
@@ -1,6 +1,9 @@
 import { WorkspaceExtensionConfiguration } from "../config";
 import { CONFIG_KEYS } from "../constants";
 
+// https://stackoverflow.com/a/3561711
+const escapeRegex = (text: string) => text.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+
 export const getApplicationId = (config: WorkspaceExtensionConfiguration) => {
     const applicationIds = new Map([
         ["Code", "782685898163617802"],
@@ -8,8 +11,7 @@ export const getApplicationId = (config: WorkspaceExtensionConfiguration) => {
         ["VSCodium", "1031067701474492496"]
     ]);
 
-    // TODO: properly escape regex
-    const appIdsRegex = new RegExp([...applicationIds.keys()].join("|"), "i");
+    const appIdsRegex = new RegExp([...Array.from(applicationIds.keys()).map(escapeRegex)].join("|"), "i");
     const match = appIdsRegex.exec(config[CONFIG_KEYS.AppName]);
 
     let clientId = config[CONFIG_KEYS.Id];
