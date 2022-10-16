d81ae13326ad5b2cd7874ea867a7d7299eb0a62d
xhayper <hayper1919@gmail.com>
2022-10-16 11:53:08 +0700

    feat: initial support for VSCodium

--- src/helpers/getApplicationId.ts
+++ src/helpers/getApplicationId.ts
@@ -2,16 +2,17 @@ import { WorkspaceExtensionConfiguration } from "../config";
 import { CONFIG_KEYS } from "../constants";
 
 export const getApplicationId = (config: WorkspaceExtensionConfiguration) => {
-    const applicationIds = new Map();
+    const applicationIds = new Map<string, string>();
 
     applicationIds.set("Code", "782685898163617802");
     applicationIds.set("Visual Studio Code", "810516608442695700");
+    applicationIds.set("VSCodium", "");
 
-    const match = /(Code|Visual Studio Code)/i.exec(config[CONFIG_KEYS.AppName]);
+    //TODO: Automatically generate regex
+    const match = /(Code|Visual Studio Code|VSCodium)/i.exec(config[CONFIG_KEYS.AppName]);
 
     let clientId = config[CONFIG_KEYS.Id];
-
-    if (match !== null && applicationIds.has(match[0])) clientId = applicationIds.get(match[0]);
+    if (match !== null && applicationIds.has(match[0])) clientId = applicationIds.get(match[0])!;
 
     return { clientId };
 };
