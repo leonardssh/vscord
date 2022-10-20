353a5b7b780680c3f1b32883e9e419d41fc3bf76
xhayper <hayper1919@gmail.com>
2022-10-20 20:56:16 +0700

    feat: properly parse application id

--- src/helpers/getApplicationId.ts
+++ src/helpers/getApplicationId.ts
@@ -1,21 +1,20 @@
 import { WorkspaceExtensionConfiguration } from "../config";
 import { CONFIG_KEYS } from "../constants";
 
-// https://stackoverflow.com/a/3561711
-const escapeRegex = (text: string) => text.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-
 export const getApplicationId = (config: WorkspaceExtensionConfiguration) => {
     const applicationIds = new Map([
-        ["Code", config.id],
+        ["Code", "782685898163617802"],
         ["Visual Studio Code", "810516608442695700"],
-        ["VSCodium", "1031067701474492496"]
+        ["VSCodium", "1031067701474492496"],
+        ["Custom", config.id]
     ]);
 
-    const appIdsRegex = new RegExp([...Array.from(applicationIds.keys()).map(escapeRegex)].join("|"), "i");
-    const match = appIdsRegex.exec(config[CONFIG_KEYS.AppName]);
-
     let clientId = config[CONFIG_KEYS.Id];
-    if (match !== null && applicationIds.has(match[0])) clientId = applicationIds.get(match[0])!;
+    for (const [appName, id] of applicationIds.entries()) {
+        if (config[CONFIG_KEYS.AppName] !== appName) continue;
+        clientId = id;
+        break;
+    }
 
     return { clientId };
 };
