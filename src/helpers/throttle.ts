fc8e5e9dfeb62063993025cd4cd6fce0283b859c
xhayper <hayper1919@gmail.com>
2022-10-22 11:13:43 +0700

    feat: refactor

--- src/helpers/throttle.ts
+++ src/helpers/throttle.ts
@@ -1,8 +1,8 @@
 export const throttle = (fn: CallableFunction, delay: number, runAfterThrottleEnd: boolean = false) => {
+    let timeout: NodeJS.Timeout | undefined;
     let lastCalled = 0;
-    let timeout: NodeJS.Timeout;
 
-    return (...args: any[]) => {
+    return (...args: any[]): any => {
         const run = () => {
             clearTimeout(timeout);
             lastCalled = new Date().getTime();
@@ -11,10 +11,10 @@ export const throttle = (fn: CallableFunction, delay: number, runAfterThrottleEn
 
         const now = new Date().getTime();
         if (now - lastCalled < delay) {
-            if (runAfterThrottleEnd) {
-                clearTimeout(timeout);
-                timeout = setTimeout(run, delay - (now - lastCalled));
-            }
+            if (!runAfterThrottleEnd) return;
+
+            clearTimeout(timeout);
+            timeout = setTimeout(run, delay - (now - lastCalled));
             return;
         }
 
