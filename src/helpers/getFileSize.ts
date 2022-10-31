ad27a79c3c6531b0cf44e86a1c271218a39c97d8
xhayper <hayper1919@gmail.com>
2022-10-31 14:01:58 +0700

    feat: fix compile error

--- src/helpers/getFileSize.ts
+++ src/helpers/getFileSize.ts
@@ -8,11 +8,11 @@ export const getFileSize = async (config: ExtenstionConfiguration, dataClass: Da
 
     let round = 2;
     if (config.get(CONFIG_KEYS.File.Size.Round) === 0 || config.get(CONFIG_KEYS.File.Size.Round))
-        round = config.get(CONFIG_KEYS.File.Size.Round);
+        round = config.get(CONFIG_KEYS.File.Size.Round)!;
 
     let spacer = " ";
     if (config.get(CONFIG_KEYS.File.Size.Spacer) === "" || config.get(CONFIG_KEYS.File.Size.Spacer))
-        spacer = config.get(CONFIG_KEYS.File.Size.Spacer);
+        spacer = config.get(CONFIG_KEYS.File.Size.Spacer)!;
 
     const fileSizeStandard: FileSizeStandard = config.get(CONFIG_KEYS.File.Size.Standard) ?? "iec";
     const fileSizeConfig: FileSizeConfig = {
