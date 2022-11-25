e35091cffa207b9e593f605158adfaceeee29e29
nick22985 <nick22985@hotmail.com>
2022-11-25 12:40:35 +1000

    cleanup

--- src/helpers/isExcluded.ts
+++ src/helpers/isExcluded.ts
@@ -1,14 +1,8 @@
 export const isExcluded = (config: string[], toMatch?: string) => {
-    console.log('here')
     if (!config || !toMatch) return false;
-    console.log('first')
     if (!config.length) return false;
-    console.log('after')
     return config.some((pattern) => {
-        console.log(pattern)
         let regex = new RegExp(pattern, "gm").test(toMatch)
-        console.log('regex', regex)
-        console.log('toWatch', toMatch)
         return regex
     });
 };
