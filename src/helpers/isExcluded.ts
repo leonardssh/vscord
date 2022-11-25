e08d54144c418377084cf3d7259130cf3fad3d89
nick22985 <nick22985@hotmail.com>
2022-11-25 12:45:11 +1000

    refactor: cleanup from testing

--- src/helpers/isExcluded.ts
+++ src/helpers/isExcluded.ts
@@ -1,8 +1,5 @@
 export const isExcluded = (config: string[], toMatch?: string) => {
     if (!config || !toMatch) return false;
     if (!config.length) return false;
-    return config.some((pattern) => {
-        let regex = new RegExp(pattern, "gm").test(toMatch)
-        return regex
-    });
+    return config.some((pattern) => new RegExp(pattern, "gm").test(toMatch));
 };
