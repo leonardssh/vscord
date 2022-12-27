ebe3460697bc52f674cd0dcc7e5b40676e6ae606
xhayper <hayper1919@gmail.com>
2022-12-27 11:58:38 +0700

    feat: improve tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     platform: "node",
+    target: ["node16", "chrome102"],
     format: "cjs",
     entry: ["src/extension.ts"],
     external: ["vscode"],
