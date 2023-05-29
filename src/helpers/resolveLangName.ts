8329abc2658eea9448a158dbb91793b9caa99591
xhayper <hayper1919@gmail.com>
2023-05-29 21:18:27 +0700

    fix: #213

--- src/helpers/resolveLangName.ts
+++ src/helpers/resolveLangName.ts
@@ -8,29 +8,36 @@ export const toUpper = <S extends string>(str: S) => str.toLocaleUpperCase() as 
 export const toTitle = <S extends string>(str: S) =>
     toLower(str).replace(/^.{1}/, (c) => toUpper(c)) as Capitalize<Lowercase<S>>;
 
+export const fileExtensionEquals = (filename: string, key: string) => {
+    if (filename.endsWith(key)) return true;
+
+    const match = /^\/(.*)\/([mgiy]+)$/.exec(key);
+    if (!match) return false;
+
+    const regex = new RegExp(match[1], match[2]);
+    return regex.test(filename);
+};
+
 export const resolveLangName = (document: TextDocument): string => {
-    const ALL_KNOWN_KNOWN_EXTENSIONS = KNOWN_EXTENSIONS;
-
-    for (const [key, value] of Object.entries(getConfig().get(CONFIG_KEYS.Behaviour.AdditionalFileMapping) ?? {}))
-        ALL_KNOWN_KNOWN_EXTENSIONS[key] = { image: value };
-
     const config = getConfig();
+
+    const ADDITIONAL_FILE_MAPPING = Object.fromEntries(
+        Object.entries(config.get(CONFIG_KEYS.Behaviour.AdditionalFileMapping)!).map(([key, value]) => [
+            key,
+            { image: value }
+        ])
+    );
+
     const filename = basename(document.fileName);
-    const findKnownExtension = Object.keys(ALL_KNOWN_KNOWN_EXTENSIONS).find((key) => {
-        if (filename.endsWith(key)) return true;
-
-        const match = /^\/(.*)\/([mgiy]+)$/.exec(key);
-        if (!match) return false;
-
-        const regex = new RegExp(match[1], match[2]);
-        return regex.test(filename);
-    });
+    const findKnownExtension =
+        Object.keys(ADDITIONAL_FILE_MAPPING).find((extension) => fileExtensionEquals(filename, extension)) ??
+        Object.keys(KNOWN_EXTENSIONS).find((extension) => fileExtensionEquals(filename, extension));
 
     const areLanguagesPrioritized = config.get(CONFIG_KEYS.Behaviour.PrioritizeLanguagesOverExtensions);
     const findKnownLanguage = KNOWN_LANGUAGES.find((key) => key.language === document.languageId);
 
     const knownExtension = findKnownExtension
-        ? ALL_KNOWN_KNOWN_EXTENSIONS[findKnownExtension]
+        ? ADDITIONAL_FILE_MAPPING[findKnownExtension] ?? KNOWN_EXTENSIONS[findKnownExtension]
         : findKnownLanguage?.image;
 
     const knownLanguage = findKnownLanguage ? findKnownLanguage.image : knownExtension;
